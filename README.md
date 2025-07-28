508fd9b25fde2dde6bcb7ac19435e1d6686bd685
Nikhil Khatwani <nikkhatwani@gmail.com>
2025-07-28 15:12:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 2024-Football-Tracker
+# Football_Ratings
---
+++ ratings.js
@@ -0,0 +1,578 @@
+const ownGoalSvg = `
+
+<svg
+width="16"
+height="16"
+viewBox="0 0 24 24"
+style="margin-right: 10px; margin-top: 3px"
+fill="var(--tertiary-default)"
+class="SvgWrapper fyGiev"
+style="min-width: 16px; display: inline-block;"
+>
+<g
+  fill="var(--tertiary-default)"
+  fill-rule="evenodd"
+  style="min-width: 16px; display: inline-block;"
+>
+  <path fill="none" d="M0 0h24v24H0z"></path>
+  <path
+    d="M22 12c0-.09-.01-.18-.01-.28C21.84 6.33 17.43 2 12 2 6.57 2 2 6.48 2 12s4.48 10 10 10 9.84-4.33 9.99-9.72c0-.09.01-.18.01-.28zm-1.94-2.67-.92-.11-.93-2.99c.82.88 1.46 1.94 1.85 3.11v-.01zm-16 3.89 2.03 2.4-.93 1.4c-.81-1.1-1.36-2.4-1.56-3.81h.46v.01zm.91-.51 1.32-3.9h3.99l1.75 3.31-1.56 3.07H7.09l-2.11-2.48h-.01zm6.22-4.39 2.41-2.44 3.62.75.98 2.97-1.71 2.8-3.48-.57-1.83-3.51h.01zm-.18-4.75 1.67 1.8-2.36 2.39H6.36l-.67-1.4v-.02a8.415 8.415 0 0 1 5.32-2.77zM5.86 17.86l1.09-1.65h3.26l2.59 2.59-.56 1.68c-.08 0-.16.01-.24.01-2.42 0-4.59-1.02-6.14-2.64v.01zm7.5 2.52.33-1 3.78-1.91.67.4a8.511 8.511 0 0 1-4.78 2.52v-.01zm5.45-3.32-1.11-.66-.53-3.14v-.01l1.87-3.03 1.3.16a8.389 8.389 0 0 1-1.53 6.68z"
+    fill="tertiary.default"
+  ></path>
+</g>
+</svg>`;
+
+const goalSvg = `
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    style="margin-right: 10px; margin-top: 3px"
+    fill="var(--secondary-default)"
+    class="SvgWrapper fyGiev"
+    style="min-width: 16px; display: inline-block;"
+  >
+    <g
+      fill="var(--secondary-default)"
+      fill-rule="evenodd"
+      style="min-width: 16px; display: inline-block;"
+    >
+      <path fill="none" d="M0 0h24v24H0z"></path>
+      <path
+        d="M22 12c0-.09-.01-.18-.01-.28C21.84 6.33 17.43 2 12 2 6.57 2 2 6.48 2 12s4.48 10 10 10 9.84-4.33 9.99-9.72c0-.09.01-.18.01-.28zm-1.94-2.67-.92-.11-.93-2.99c.82.88 1.46 1.94 1.85 3.11v-.01zm-16 3.89 2.03 2.4-.93 1.4c-.81-1.1-1.36-2.4-1.56-3.81h.46v.01zm.91-.51 1.32-3.9h3.99l1.75 3.31-1.56 3.07H7.09l-2.11-2.48h-.01zm6.22-4.39 2.41-2.44 3.62.75.98 2.97-1.71 2.8-3.48-.57-1.83-3.51h.01zm-.18-4.75 1.67 1.8-2.36 2.39H6.36l-.67-1.4v-.02a8.415 8.415 0 0 1 5.32-2.77zM5.86 17.86l1.09-1.65h3.26l2.59 2.59-.56 1.68c-.08 0-.16.01-.24.01-2.42 0-4.59-1.02-6.14-2.64v.01zm7.5 2.52.33-1 3.78-1.91.67.4a8.511 8.511 0 0 1-4.78 2.52v-.01zm5.45-3.32-1.11-.66-.53-3.14v-.01l1.87-3.03 1.3.16a8.389 8.389 0 0 1-1.53 6.68z"
+        fill="secondary.default"
+      ></path>
+    </g>
+  </svg>`;
+
+const assistSvg = `
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="14"
+    style="margin-right: 10px; margin-top: 3px"
+    height="14"
+    viewBox="0 0 14 14"
+  >
+    <ellipse cx="7" cy="7" rx="7" ry="7" fill="transparent"></ellipse>
+    <g id="ic_assist" transform="translate(0 0)">
+      <path
+        id="Path_88"
+        fill="var(--secondary-default)"
+        fill-rule="evenodd"
+        d="M12.608 5.7c-.175.1-.377.209-.6.337-.156.09-.322.188-.493.3-.806.524-6.651 4.113-7.836 4.793s-3.035.929-3.565.016 1.029-1.952 1.948-3.055C3.11 6.833 4.48 5.461 4.48 5.461c-.088-.426.332-.712.494-.805a.607.607 0 0 1 .06-.03c-.117-.5.631-.929.631-.929l1.147-2.518a.231.231 0 0 1 .094-.105.236.236 0 0 1 .208-.013l1.024.424c.673.283-.769 1.89-.465 1.962a1.67 1.67 0 0 0 1.043-.273 2.826 2.826 0 0 0 .735-.614c.48-.56-.03-1.38.249-1.543.1-.054.287-.034.642.095 1.393.535 2.192 2.211 2.776 3.254.402.709.121.973-.51 1.334zm-8.018.693a.085.085 0 0 0-.075.022l-.631.62a.079.079 0 0 0 .04.135l3.227.669a.09.09 0 0 0 .058-.009l.981-.563a.081.081 0 0 0-.02-.15zm5.558-.418l-4.407-.844a.089.089 0 0 0-.075.023l-.628.618a.081.081 0 0 0 .041.137l3.99.807a.089.089 0 0 0 .058-.009l1.041-.581a.082.082 0 0 0-.02-.151zM3.807 12.1a.083.083 0 0 1-.039.1l-.734.422a.082.082 0 0 1-.1-.016l-.546-.579a.083.083 0 0 1-.016-.063 5.312 5.312 0 0 0 1.3-.462zm1.668-.92a.083.083 0 0 1-.039.1l-.736.42a.082.082 0 0 1-.1-.016l-.41-.484c.3-.177.693-.415 1.15-.691zm5.687-3.4a.084.084 0 0 1-.039.1l-.735.422a.082.082 0 0 1-.1-.016l-.488-.5c.441-.27.839-.516 1.158-.716zM12.5 6.132c.1-.052.184-.1.268-.154L12.9 5.9l.222.754a.084.084 0 0 1-.039.1l-.734.422a.082.082 0 0 1-.1-.016L11.7 6.6c.144-.093.294-.182.466-.281.118-.068.224-.129.334-.187z"
+      ></path>
+    </g>
+  </svg>
+`;
+
+const calculateRating = (playerStats, pos) => {
+  let rating = 6;
+
+  const baseWeights = {
+    totalPass: 0.001,
+    accuratePass: 0.002,
+    totalLongBalls: 0.01,
+    accurateLongBalls: 0.03,
+    totalCross: 0.005,
+    accurateCross: 0.02,
+    aerialLost: -0.03,
+    aerialWon: 0.03,
+    duelLost: -0.03,
+    duelWon: 0.03,
+    challengeLost: -0.05,
+    dispossessed: -0.05,
+    totalContest: 0.015,
+    wonContest: 0.03,
+    bigChanceCreated: 0.2,
+    bigChanceMissed: -0.1,
+    shotOffTarget: -0.02,
+    onTargetScoringAttempt: 0.1,
+    blockedScoringAttempt: 0.05,
+    outfielderBlock: 0.03,
+    goals: 0.6,
+    goalAssist: 0.4,
+    hitWoodwork: 0.1,
+    totalClearance: 0.03,
+    totalTackle: 0.03,
+    wasFouled: 0.03,
+    fouls: -0.05,
+    totalOffside: -0.02,
+    touches: 0.005,
+    possessionLostCtrl: -0.03,
+    expectedGoals: 0.1,
+    keyPass: 0.1,
+    expectedAssists: 0.1,
+    interceptions: 0.03,
+    interceptionWon: 0.05,
+    ownGoals: -1.7,
+    penaltyConceded: -0.3,
+    penaltyShootoutGoals: 0.05,
+
+    saves: 0.03,
+    goalsPrevented: 0.05,
+    punches: 0.03,
+    savedShotsFromInsideTheBox: 0.05,
+    totalKeeperSweeper: 0.03,
+    accurateKeeperSweeper: 0.05,
+    errorLeadToAGoal: -1,
+  };
+
+  const positionModifiers = {
+    G: {
+      saves: 0.05,
+      goalsPrevented: 0.1,
+      punches: 0.05,
+      savedShotsFromInsideTheBox: 0.1,
+      totalKeeperSweeper: 0.05,
+      accurateKeeperSweeper: 0.1,
+      errorLeadToAGoal: -1.3,
+    },
+    D: {
+      totalClearance: 0.05,
+      totalTackle: 0.05,
+      interceptions: 0.05,
+      aerialWon: 0.05,
+      aerialLost: -0.05,
+      duelWon: 0.05,
+      duelLost: -0.05,
+    },
+    M: {
+      accuratePass: 0.005,
+      totalPass: 0.003,
+      keyPass: 0.15,
+      accurateLongBalls: 0.05,
+      accurateCross: 0.03,
+      bigChanceCreated: 0.3,
+    },
+    F: {
+      goals: 0.8,
+      onTargetScoringAttempt: 0.2,
+      shotOffTarget: -0.05,
+      bigChanceMissed: -0.1,
+      goalAssist: 0.5,
+      bigChanceCreated: 0.3,
+    },
+  };
+
+  const dynamicWeights = { ...baseWeights, ...(positionModifiers[pos] || {}) };
+
+  if (playerStats.goals > 1) {
+    dynamicWeights.goals *= 1.5;
+  }
+  if (playerStats.goals > 2) {
+    dynamicWeights.goals *= 2;
+  }
+  if (playerStats.saves >= 5) {
+    dynamicWeights.saves *= 1.5;
+  }
+  if (playerStats.saves >= 8) {
+    dynamicWeights.saves *= 2;
+  }
+  if (playerStats.goalAssist > 1) {
+    dynamicWeights.goalAssist *= 1.4;
+  }
+  if (playerStats.goalAssist > 2) {
+    dynamicWeights.goalAssist *= 1.8;
+  }
+  if (playerStats.goals > 0 && playerStats.goalAssist > 0) {
+    // dynamicWeights.goals *= 1.5;
+    // dynamicWeights.goalAssist *= 1.5;
+  }
+
+  for (const stat in playerStats) {
+    if (dynamicWeights[stat]) {
+      rating += playerStats[stat] * dynamicWeights[stat];
+    }
+  }
+
+  rating = Math.max(3.0, Math.min(rating, 10.0));
+
+  return rating;
+};
+
+const getDifferentTournaments = async (
+  season_id,
+  tournament_id,
+  isPast,
+  page = 0
+) => {
+  const tournamentIDs = [];
+  const past = await fetch(
+    `https://www.sofascore.com/api/v1/unique-tournament/${tournament_id}/season/${season_id}/events/last/${page}`
+  );
+  const pastData = await past.json();
+  const upcoming = await fetch(
+    `https://www.sofascore.com/api/v1/unique-tournament/${tournament_id}/season/${season_id}/events/next/${page}`
+  );
+  const upcomingData = await upcoming.json();
+
+  if (isPast) {
+    pastData.events.forEach((event) => {
+      tournamentIDs.push(event.id);
+    });
+
+    return {
+      tournamentIDs,
+      data: pastData,
+    };
+  } else {
+    upcomingData.events.forEach((event) => {
+      tournamentIDs.push(event.id);
+    });
+
+    return {
+      tournamentIDs,
+      data: upcomingData,
+    };
+  }
+};
+
+const getPlayers = async (tID) => {
+  const fullPlayer = [];
+
+  const response = await fetch(
+    `https://www.sofascore.com/api/v1/event/${tID}/lineups`
+  );
+  const data = await response.json();
+  console.log(data);
+  const homeLineup = data.home.players;
+  const awayLineup = data.away.players;
+
+  homeLineup.forEach((player) => {
+    // if (player.substitute) return;
+    fullPlayer.push(player);
+  });
+  awayLineup.forEach((player) => {
+    // if (player.substitute) return;
+    fullPlayer.push(player);
+  });
+
+  return {
+    fullPlayer,
+    data,
+  };
+};
+
+const handleLoadPlayers = async (tournament, match) => {
+  const lineup_match = document.getElementsByClassName("lineup-match")[0];
+  if (lineup_match) {
+    lineup_match.remove();
+  }
+  const players_container =
+    document.getElementsByClassName("players-container")[0];
+  if (players_container) {
+    players_container.remove();
+  }
+
+  const lineup = document.getElementsByClassName("lineups")[0];
+  const lineup_header = document.getElementsByClassName("lineups-header")[0];
+  const newDiv = document.createElement("div");
+
+  newDiv.classList.add("lineup-match");
+  lineup_header.appendChild(newDiv);
+  newDiv.textContent = `${tournament.homeTeam.name} vs ${tournament.awayTeam.name}`;
+
+  const playersContainer = document.createElement("div");
+  playersContainer.classList.add("players-container");
+  lineup.appendChild(playersContainer);
+
+  const { fullPlayer } = await getPlayers(tournament.id);
+  const allPlayerStats = [];
+  const playersInOrder = [];
+
+  fullPlayer.forEach((player) => {
+    if (player.statistics.minutesPlayed == undefined) {
+      return;
+    }
+
+    allPlayerStats.push(player);
+  });
+
+  for (const playerStats of allPlayerStats) {
+    const stats = playerStats.statistics;
+    const rating = calculateRating(stats, playerStats.position);
+    playersInOrder.push({
+      player: playerStats,
+      rating,
+    });
+  }
+
+  playersInOrder.sort((a, b) => b.rating - a.rating);
+
+  playersInOrder.forEach((player) => {
+    const playerStats = player.player.player;
+    const teamId =
+      playerStats.country.name === tournament.homeTeam.name
+        ? tournament.homeTeam.id
+        : tournament.awayTeam.id;
+
+    const playerDiv = document.createElement("div");
+    playerDiv.classList.add("player");
+    playersContainer.appendChild(playerDiv);
+
+    const playerNationality = document.createElement("div");
+    playerNationality.classList.add("player-nationality");
+    playerDiv.appendChild(playerNationality);
+
+    const playerNationalityImage = document.createElement("img");
+    playerNationalityImage.src = `https://api.sofascore.app/api/v1/team/${teamId}/image`;
+    playerNationalityImage.alt = "Team";
+    playerNationality.appendChild(playerNationalityImage);
+
+    // const playerJerseyNumber = document.createElement("div");
+    // playerJerseyNumber.classList.add("player-jersey-number");
+    // playerJerseyNumber.textContent = playerStats.jerseyNumber;
+    // playerDiv.appendChild(playerJerseyNumber);
+
+    const playerImage = document.createElement("img");
+    playerImage.src = `https://api.sofascore.app/api/v1/player/${playerStats.id}/image`;
+    playerImage.alt = "Player";
+    playerImage.classList.add("player-image");
+    playerDiv.appendChild(playerImage);
+
+    const playerName = document.createElement("p");
+    let playerNameText = `${playerStats.name} ${
+      player.player.captain ? "(C)" : ""
+    } ${playerStats.position == "G" ? "(GK)" : ""}`;
+
+    playerName.classList.add("player-name");
+    playerName.textContent = playerNameText;
+    playerDiv.appendChild(playerName);
+
+    if (player.player.statistics.ownGoals > 0) {
+      const ownGoal = document.createElement("span");
+      ownGoal.innerHTML = ownGoalSvg;
+      playerDiv.appendChild(ownGoal);
+    }
+
+    for (let i = 0; i < player.player.statistics.goals; i++) {
+      const goal = document.createElement("span");
+      goal.innerHTML = goalSvg;
+      playerDiv.appendChild(goal);
+    }
+
+    for (let i = 0; i < player.player.statistics.goalAssist; i++) {
+      const assist = document.createElement("span");
+      assist.innerHTML = assistSvg;
+      playerDiv.appendChild(assist);
+    }
+
+    const playerRating = document.createElement("p");
+    if (player.rating.toFixed(1) >= 9.0) {
+      playerRating.classList.add("amazing");
+    } else if (player.rating.toFixed(1) >= 8.0) {
+      playerRating.classList.add("great");
+    } else if (player.rating.toFixed(1) >= 7.0) {
+      playerRating.classList.add("good");
+    } else if (player.rating.toFixed(1) >= 6.0) {
+      playerRating.classList.add("average");
+    } else if (player.rating.toFixed(1) >= 5.0) {
+      playerRating.classList.add("bad");
+    } else {
+      playerRating.classList.add("very-bad");
+    }
+
+    playerRating.classList.add("player-rating");
+    playerRating.textContent = player.rating.toFixed(1);
+    playerDiv.appendChild(playerRating);
+  });
+};
+
+const main = async (euros, past = true, page = 0) => {
+  const allMatches = document.querySelectorAll(".match");
+  allMatches.forEach((match) => {
+    match.remove();
+  });
+  const header = document.getElementById("header-left");
+  while (header.firstChild) {
+    header.removeChild(header.firstChild);
+  }
+
+  let season_id = euros ? 56953 : 57114;
+  let tournament_id = euros ? 1 : 133;
+
+  let { tournamentIDs, data: tournamentData } = await getDifferentTournaments(
+    season_id,
+    tournament_id,
+    past,
+    page
+  );
+
+  const headerImg = document.createElement("img");
+  headerImg.src = `https://api.sofascore.app/api/v1/unique-tournament/${tournamentData.events[0].tournament.uniqueTournament.id}/image`;
+  headerImg.alt = "Tournament";
+  header.appendChild(headerImg);
+  const headerText = document.createElement("h1");
+  headerText.textContent =
+    tournamentData.events[0].tournament.uniqueTournament.name;
+  header.appendChild(headerText);
+
+  if (past) tournamentIDs = tournamentIDs.reverse();
+
+  tournamentIDs.forEach(async (tournamentID) => {
+    const tournament = tournamentData.events.find(
+      (event) => event.id === tournamentID
+    );
+    const { homeTeam, homeScore, awayTeam, awayScore } = tournament;
+    const group = tournament.tournament.groupName;
+
+    const homeImage = `https://api.sofascore.app/api/v1/team/${homeTeam.id}/image`;
+    const awayImage = `https://api.sofascore.app/api/v1/team/${awayTeam.id}/image`;
+
+    const match = document.createElement("div");
+    match.classList.add("match");
+    match.addEventListener("click", (e) => {
+      const allMatches = document.querySelectorAll(".match");
+      allMatches.forEach((match) => {
+        match.classList.remove("selected");
+      });
+
+      match.classList.toggle("selected");
+
+      handleLoadPlayers(tournament, match);
+    });
+    document.getElementById("matches-container").appendChild(match);
+
+    const homeTeamDiv = document.createElement("div");
+    homeTeamDiv.classList.add("team");
+    if (homeScore.current < awayScore.current) {
+      homeTeamDiv.classList.add("lost");
+    }
+    match.appendChild(homeTeamDiv);
+
+    const homeImageElement = document.createElement("img");
+    homeImageElement.src = homeImage;
+    homeImageElement.alt = homeTeam.name;
+    homeTeamDiv.appendChild(homeImageElement);
+
+    const homeTeamName = document.createElement("h3");
+    homeTeamName.textContent = homeTeam.name;
+    homeTeamDiv.appendChild(homeTeamName);
+
+    const scoreDiv = document.createElement("div");
+    scoreDiv.classList.add("score");
+    match.appendChild(scoreDiv);
+
+    const homeScoreP = document.createElement("p");
+    homeScoreP.textContent = homeScore.current;
+    if (homeScore.current > awayScore.current) {
+      homeScoreP.style.fontWeight = "bold";
+    }
+    scoreDiv.appendChild(homeScoreP);
+
+    const dashP = document.createElement("p");
+    dashP.textContent = "-";
+    scoreDiv.appendChild(dashP);
+
+    const awayScoreP = document.createElement("p");
+    awayScoreP.textContent = awayScore.current;
+    if (homeScore.current < awayScore.current) {
+      awayScoreP.style.fontWeight = "bold";
+    }
+    scoreDiv.appendChild(awayScoreP);
+
+    const awayTeamDiv = document.createElement("div");
+    awayTeamDiv.classList.add("team");
+    awayTeamDiv.classList.add("away");
+    if (awayScore.current < homeScore.current) {
+      awayTeamDiv.classList.add("lost");
+    }
+    match.appendChild(awayTeamDiv);
+
+    const awayTeamName = document.createElement("h3");
+    awayTeamName.textContent = awayTeam.name;
+    awayTeamDiv.appendChild(awayTeamName);
+
+    const awayImageElement = document.createElement("img");
+    awayImageElement.src = awayImage;
+    awayImageElement.alt = awayTeam.name;
+    awayTeamDiv.appendChild(awayImageElement);
+
+    const groupDiv = document.createElement("div");
+    groupDiv.classList.add("group");
+    groupDiv.textContent = group;
+    match.appendChild(groupDiv);
+  });
+};
+
+const toggle = document.getElementById("toggle");
+const pTag = document.getElementById("switch-p");
+const span = document.getElementById("switch-span");
+
+const selector = document.getElementById("matches-container-selector");
+const options = document.querySelectorAll(".matches-container-option");
+
+let changeTimeout = null;
+
+const updateStyles = () => {
+  if (!toggle.checked) {
+    pTag.style.fontWeight = "bold";
+    span.style.fontWeight = "normal";
+  } else {
+    pTag.style.fontWeight = "normal";
+    span.style.fontWeight = "bold";
+  }
+};
+
+const handleChange = () => {
+  clearTimeout(changeTimeout);
+  changeTimeout = setTimeout(() => {
+    if (!toggle.checked) {
+      if (options[0].classList.contains("active")) {
+        main(true, true);
+      } else {
+        main(true, false);
+      }
+    } else {
+      if (options[0].classList.contains("active")) {
+        main(false, true);
+      } else {
+        main(false, false);
+      }
+    }
+  }, 100);
+};
+
+updateStyles();
+handleChange();
+toggle.addEventListener("change", () => {
+  updateStyles();
+  handleChange();
+});
+
+const changeMatches = (type, page = 0) => {
+  const isPast = type === "past";
+  if (isPast) {
+    main(!toggle.checked, true, page);
+  } else {
+    main(!toggle.checked, false, page);
+  }
+};
+
+options.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    options.forEach((option) => {
+      option.classList.remove("active");
+    });
+
+    option.classList.add("active");
+  });
+});
+
+const pagesButton = document.getElementsByClassName("pages-button");
+const left = pagesButton[0];
+const right = pagesButton[1];
+
+left.addEventListener("click", () => {
+  const active = document.getElementsByClassName("active")[0];
+  const type = active.textContent.toLowerCase();
+  const typeWithoutSpace = type.replace(/\s/g, "");
+  changeMatches(typeWithoutSpace, 0);
+});
+
+right.addEventListener("click", () => {
+  const active = document.getElementsByClassName("active")[0];
+  const type = active.textContent.toLowerCase();
+  const typeWithoutSpace = type.replace(/\s/g, "");
+  changeMatches(typeWithoutSpace, 1);
+});
